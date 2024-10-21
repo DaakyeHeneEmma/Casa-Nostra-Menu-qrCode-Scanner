56423c2027dbb7d4f8de8f2ec0a4a50e4a1df2be
Emmanuel Arthur <earthur@tapnhop.com>
2024-10-21 00:55:21 +0000

    arranging files

--- src/app/test.js
+++ src/app/test.js
@@ -1,10 +1,20 @@
 const ArrayData = (data) => {
-    let newdata = 0
+    // let newdata = 0
 
-    for(let i = 0; i < data.length; i++){
-        data[i] < newdata ? (newdata = data[i]): newdata
-    }
-    console.log(newdata)
+    // for(let i = 0; i < data.length; i++){
+    //     data[i] < newdata ? (newdata = data[i]): newdata
+    // }
+    // console.log(newdata)
+
+         data.filter((item, index)=>(
+         console.log( index, item)
+    ))
+
+    Array.from({length:3}).map((item, index, other)=>(
+     
+        console.log(item, index, 
+            (other.map((image)=> image)))
+    ))
 }
 
 ArrayData([1,2,3,54])
